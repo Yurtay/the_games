a7cec70149e8c4baacce989111704663792d4f77
Igor <yurtay2017@yandex.ru>
2024-02-07 09:50:24 +0500

    CHECK WORK

--- src/componets/mainPage.jsx
+++ src/componets/mainPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import dice from "../img/dice.jpg";
+import tic_tac_toe from "../img/tic_tac_toe.jpg";
 
 const Main = () => {
   return (
     <>
       <Link to="dice">
-        <div className="container_game">DICE</div>
+        <div className="container_game">
+          <img src={dice} alt="dice" />
+        </div>
       </Link>
       <Link to="tic_tac_toe">
-        <div className="container_game">TIC TAC TOE</div>
+        <div className="container_game">
+          <img src={tic_tac_toe} alt="tic_tac_toe" />
+        </div>
       </Link>
     </>
   );
